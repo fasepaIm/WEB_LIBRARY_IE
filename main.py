4fa8496d868753d703a13640773580bda3be60ad
fasepaIm <puh.egor@yandex.ru>
2021-05-30 20:13:54 +0300

    some changes

--- main.py
+++ main.py
@@ -173,6 +173,7 @@ def logout():
 
 
 if __name__ == '__main__':
-    db_session.global_init("db/blogs.db")
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
+    db_session.global_init(os.path.join(os.path.abspath(os.path.dirname(__file__)), f'db/blogs.db'))
+    #port = int(os.environ.get("PORT", 5000))
+    #app.run(host='0.0.0.0', port=port)
+    app.run()
